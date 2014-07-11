8542d0ab760612bc840f363d1a305c851f754973
Oleksandr Knyga <oleksandrknyga@gmail.com>
2014-07-11 17:57:42 +0300

    pos partial fix

--- isotope.stick.ext.js
+++ isotope.stick.ext.js
@@ -9,33 +9,67 @@
 		this.each(function() {
 			var $this = $(this);
 			var $items = $this.children();
-			var itemWidth = -1;
+			 var itemMinWidth = -1;
 
-			var calculateElementWidth = function() {
-				if(-1 == itemWidth) {
-					itemWidth = parseFloat($items.first().outerWidth(true));
+			var calculateElementMinWidth = function() {
+				if(-1 == itemMinWidth) {
+					itemMinWidth = $items.first().outerWidth(true);
+					$items.each(function(item) {
+						var width = $(this).outerWidth(true);
+
+						if(width < itemMinWidth) {
+							itemMinWidth = width;
+						}
+					});
+					itemMinWidth = parseFloat(itemMinWidth);
 				}
-				return itemWidth;
+				return itemMinWidth;
 			};
 			var calculateParentWidth = function() {
 				return parseFloat($this.width());
 			};
 			var getRightPosition = function() {
-				console.log(calculateParentWidth() / calculateElementWidth() - 1);
-				return Math.floor(calculateParentWidth() / calculateElementWidth() - 1);
+				// var pos = 0,
+				// 	sumWidth = 0,
+				// 	pw = calculateParentWidth() - calculateElementMinWidth(),
+				// 	biggerThanMinSum = 0;
+
+				// $items.each(function(i) {
+				// 	if(sumWidth <= pw) {
+				// 		width = $(this).outerWidth(true);
+				// 		sumWidth += width;
+				// 		pos = i;
+
+				// 		console.log(width, $(this).attr('data-pos'));
+
+				// 		if(sumWidth <= pw) {
+				// 			if(width > calculateElementMinWidth()) {
+				// 				biggerThanMinSum += (width - calculateElementMinWidth());
+				// 			}
+				// 		}
+				// 	}
+				// });
+
+				// return pos - 1;
+
+				return Math.floor(calculateParentWidth() / calculateElementMinWidth() - 1);
 			};
 			var reindexPositions = function() {
 				var rtp = getRightPosition();
 
 				for(var i=0, j = 0;i<$items.length;i++) {
+					if(i < rtp && $items.eq(i).outerWidth(true) > calculateElementMinWidth()) {
+						rtp -= 1;
+					}
+
 					if(rtp == i) {
 						j++;
 					}
 
-					$items.eq(i).attr('data-pos', i+j);
+					$items.eq(i).attr('data-pos', i+j).text(i+j);
 				}
 
-				$this.find('[data-align="right-top"]').attr('data-pos', rtp);
+				$this.find('[data-align="right-top"]').attr('data-pos', rtp).text(rtp);
 			};
 
 			reindexPositions();
